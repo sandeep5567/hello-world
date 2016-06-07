34ac62f53026ec50c45312b320ab6d1937a51425
sandeep5567 <sandeep.sandy575@gmail.com>
2016-06-07 07:15:25 +0000

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,27 @@
+package com.niit.shoppingcard.bean;
+
+public class Product {
+	private int id;
+	private int name;
+	private int price;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getName() {
+		return name;
+	}
+	public void setName(int i) {
+		this.name = i;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+
+}
---
+++ Test.java
@@ -0,0 +1,14 @@
+package com.niit.shoppingcard.bean;
+
+public class Test {
+public static void main (String [] args)
+{
+Product product1=new Product();
+product1.setId(101);
+product1.setName(54);
+product1.setPrice(52);
+System.out.println("ID number is:"+product1.getId());
+System.out.println("product name is:"+product1.getName());
+System.out.println("product price is:"+product1.getPrice());
+}
+}
